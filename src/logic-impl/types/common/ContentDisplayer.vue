4dd86e6bca3613f4145739a705bf419fc7be5026
Trigary <trigary0@gmail.com>
2024-02-25 15:31:51 +0100

    add markdown support

--- src/logic-impl/types/common/ContentDisplayer.vue
+++ src/logic-impl/types/common/ContentDisplayer.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import type { DisplayableContent } from '@/logic/entity'
+import md from '@/plugins/text-rendering'
+import { ref } from 'vue'
 
 const props = defineProps<{
   content: DisplayableContent
 }>()
+
+const content = ref(md.render(props.content.rawText))
 </script>
 
 <template>
-  {{ props.content.rawText }}
+  <div v-html="content" />
 </template>
 
 <style scoped></style>
---
+++ src/plugins/text-rendering.ts
@@ -0,0 +1,7 @@
+import MarkdownIt from 'markdown-it'
+
+const md = MarkdownIt()
+
+//TODO configure e.g. syntax highlighting
+
+export default md
